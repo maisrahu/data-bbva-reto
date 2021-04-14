b1172ac120aec2caff04f0fa0cbcb940e91d76e9
alejlatorre <alej669@gmail.com>
2021-04-14 13:32:28 -0500

    modified rcc_cal_gral

--- utils/simulator.py
+++ utils/simulator.py
@@ -3,15 +3,20 @@ import numpy as np
 import math
 import random
 import requests 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-#from bs4 import BeautifulSoup
+import os
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 
-# r_obj = requests.Session()
-# url = 'http://clientes.serpost.com.pe/prj_pjudicial/ubigeo.aspx'
-# fr_soup = r_obj.get(url)
-# soup = BeautifulSoup(fr_soup.content, 'lmxl')
-# print(soup)
+
+# path = os.getcwd()
+# print(os.path.abspath(os.path.join(path, os.pardir)))
+
+# data_path =  'Data\\' 
+# filename = 'ubigeo.csv'
+# df_ubigeo = pd.read_csv(data_path + filename, sep=';')
+
+# print(df_ubigeo)
+
 
 def simulate(n=100, seed=None):
     if seed==None:
@@ -22,24 +27,41 @@ def simulate(n=100, seed=None):
     dni = random.sample(range(n), n)
     edad = [int(np.random.triangular(18, 35, 75)) for i in range(n)]
     # correo = ['']
-    rcc_cal_gral = np.random.choice( a=['RCC1', 'RCC2', 'RCC3', 'RCC4', 'RCC5'], size=n, p=[0.1, 0.3, 0.4, 0.1, 0.1] )
+    rcc_cal_gral = np.random.choice( 
+        a=["OK", "CPP", "PER", "DUD", "DEF"], 
+        size=n, 
+        p=[0.1, 0.3, 0.4, 0.1, 0.1] )
     rcc_num_ent_sbs = [int(np.random.triangular(1, 3, 10)) for i in range(n)]
     rcc_linea_tc_max = np.random.exponential(scale=1000, size=n)
     rcc_nro_prod = [int(np.random.triangular(1, 7, 20)) for i in range(n)]
     num_seguros_salud = [int(np.random.triangular(1, 1, 3)) for i in range(n)]
     ingreso_mens = np.random.exponential(scale=1000, size=n)
-    es_cliente = np.random.choice( a=['0','1'], size=n, p=['0.8', '0.2'])
-    flg_viaje_tw = np.random.choice( a=['0','1'], size=n, p=['0.7', '0.3'])
-    tipo_vivienda = np.random.choice( a=['propia', 'hipot', 'alqui', 'fami', 'otro'], size=n, p=[0.1, 0.3, 0.4, 0.1, 0.1])
-    sit_laboral = np.random.choice( a=['DEP','INDEP'], size=n, p=[0.6, 0.4]) 
-    segmento = np.random.choice( a=['SEGM1', 'SEGM2', 'SEGM3','SEGM4'],   size=n,    p=[0.1, 0.3, 0.4, 0.2]  )
+    es_cliente = np.random.choice( 
+        a=['0','1'], 
+        size=n, 
+        p=['0.8', '0.2'])
+    flg_viaje_tw = np.random.choice( 
+        a=['0','1'], 
+        size=n, 
+        p=['0.7', '0.3'])
+    tipo_vivienda = np.random.choice( 
+        a=['propia', 'hipot', 'alqui', 'fami', 'otro'], 
+        size=n, 
+        p=[0.1, 0.3, 0.4, 0.1, 0.1])
+    sit_laboral = np.random.choice( 
+        a=['DEP','INDEP'], 
+        size=n, 
+        p=[0.6, 0.4]) 
+    segmento = np.random.choice( 
+        a=['SEGM1', 'SEGM2', 'SEGM3','SEGM4'],   
+        size=n,    
+        p=[0.1, 0.3, 0.4, 0.2]  )
     prod_interes = np.random.choice(
         a=['CTA_AHORRO','TC','T_CAMBIO','PREST', 'ADS', 'TA', 'DP', 'FM'], 
         size=n, 
         p=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.3])
-     
+    
     # tasa = [np.random.triangular(3.0, 3.2, 3.6) for i in range(n)]
-
     d = {
         'dni': dni, 
         'edad': edad,
@@ -56,7 +78,6 @@ def simulate(n=100, seed=None):
         'sit_laboral': sit_laboral,
         'segmento': segmento,
         'prod_interes': prod_interes}
-
     df = pd.DataFrame(data=d)
     
     return df
