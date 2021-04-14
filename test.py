ac18c1a32297e2fd7f842fcd4e9ff73eb89fa303
alejlatorre <alejandro.latorre@pucp.edu.pe>
2021-04-14 14:51:31 -0500

    requirements updated

--- test.py
+++ test.py
@@ -3,13 +3,9 @@ import numpy as np
 import matplotlib as plt
 import utils.simulator as simu 
 
-from sklearn import tree
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, max_error
+from sklearn.clusters import KMeans
+from sklearn.metrics import pairwise_distances_argmin_min
 
 
 df = simu.simulate(1000)
 print(df)
-
